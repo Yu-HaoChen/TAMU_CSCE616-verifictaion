36e4a332f631602534749130eeb984c6ad033608
Yu-HaoChen <harrison900531@tamu.edu>
2025-02-21 23:10:06 -0600

    Add files via upload

---
+++ lab-0-Yu-HaoChen/work/sim/xcelium.d/run.lnx86.22.03.d/files.ts
@@ -0,0 +1,2 @@
+1724474508 /home/grads/h/harrison900531/lab-0-Yu-HaoChen/work/design/common/addr_segregator_proc.sv
+1724561227 /home/grads/h/harrison900531/lab-0-Yu-HaoChen/work/tb/tb_addr_segregator_proc.sv
